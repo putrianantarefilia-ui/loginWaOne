79e1a3722ae835c0c5c140c368dd94c79fc32ad4
putrianantarefilia-ui <putrianantarefilia@gmail.com>
2026-05-12 11:11:23 +0700

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/loginWaOne',
-  trailingSlash: true,
+  assetPrefix: '/loginWaOne/',
   images: {
     unoptimized: true,
     remotePatterns: [
