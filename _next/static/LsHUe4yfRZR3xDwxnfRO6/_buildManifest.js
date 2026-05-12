1cbceb4670e27370f94a1c7c570d3ae1af9b5118
putrianantarefilia-ui <putrianantarefilia@gmail.com>
2026-05-12 09:59:52 +0700

    Updates

--- _next/static/LsHUe4yfRZR3xDwxnfRO6/_buildManifest.js
+++ _next/static/LsHUe4yfRZR3xDwxnfRO6/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/loginWaOne//_next/:path+",
+        "destination": "/loginWaOne/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
