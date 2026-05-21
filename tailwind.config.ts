68ab37a0bb92809c67f25c85cfba4637d8d3e696
putrianantarefilia-ui <putrianantarefilia@gmail.com>
2026-05-21 09:34:20 +0700

    refactor: remove PostCSS configuration and switch to ES module import for tailwindcss-forms

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import forms from "@tailwindcss/forms";
 
 const config: Config = {
   darkMode: "class",
@@ -91,7 +92,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [forms],
 };
 
 export default config;
