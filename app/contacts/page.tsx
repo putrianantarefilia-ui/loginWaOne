f36de2a9b61dce8829d07ee6d25d11999f4a9d21
putrianantarefilia-ui <putrianantarefilia@gmail.com>
2026-06-18 16:51:28 +0700

    ubah logo logout

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -466,7 +466,7 @@ export default function ContactsPage() {
               viewBox="0 0 24 24"
             >
               <path
-                d="M17 16l4-4m0 0l-4-4"
+                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
